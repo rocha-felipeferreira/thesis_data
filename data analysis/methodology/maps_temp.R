92747d903da159cd2b1c63c48db45c1c1d1ba6d2
Felipe Rocha <rocha.felipeferreira@gmail.com>
2020-04-29 20:18:23 -0300

    working on maps yet

--- data analysis/methodology/maps_temp.R
+++ data analysis/methodology/maps_temp.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 library(stringr)
+library(ggthemes)
 
 mapa_mundo <- map_data("world")
 
@@ -11,9 +12,13 @@ quantd_disc_mre$region <- quantd_disc_mre$country
 
 quantd_disc_mre <- quantd_disc_mre %>% select(4, 5)
 
-teste <- inner_join(quantd_disc_mre, mapa_mundo)
+teste <- right_join(quantd_disc_mre, mapa_mundo)
+
+teste <- mutate(teste, n = ifelse(is.na(n), 0, n))
+
 
 ggplot(teste, aes(x = long, y = lat, group = group, fill = n)) +
-  geom_polygon()
+  geom_polygon(colour = "black") + theme_void()
+
 
 #Resolver problemas dos paises missings, colocar zero. 
