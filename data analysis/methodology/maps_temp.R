93490c2e7104b71b4135095e9eb9880fb53d37ba
Felipe Rocha <rocha.felipeferreira@gmail.com>
2020-04-30 10:48:01 -0300

    Finished creating the raw structure of the map, now I can keep doing this and the Presidents maps and then merge them and then delete this file and merge with the original

--- data analysis/methodology/maps_temp.R
+++ data analysis/methodology/maps_temp.R
@@ -1,8 +1,10 @@
 library(tidyverse)
 library(stringr)
 library(ggthemes)
+library(viridis)
 
 mapa_mundo <- map_data("world")
+mapa_mundo <- filter(mapa_mundo, region != "Antarctica")
 
 quantd_disc <- read_csv("data analysis/methodology/country_speeches.csv")
 
@@ -16,9 +18,14 @@ teste <- right_join(quantd_disc_mre, mapa_mundo)
 
 teste <- mutate(teste, n = ifelse(is.na(n), 0, n))
 
+teste$brk <- cut(teste$n, 
+                    breaks = c(-1, 0, 15, 50, 585), 
+                    labels = c("0", "1 - 14", "15 - 49", "50 - 585"))
 
-ggplot(teste, aes(x = long, y = lat, group = group, fill = n)) +
-  geom_polygon(colour = "black") + theme_void()
+
+ggplot(teste, aes(x = long, y = lat, group = group, fill = brk)) +
+  geom_polygon(colour = "black", size = .2, alpha = .5) + theme_void() +
+scale_fill_manual(values = c("0" = "gray80", "1 - 14" = "orange", "15 - 49" = "darkgreen", "50 - 585" = "blue")) 
 
 
-#Resolver problemas dos paises missings, colocar zero. 
+s
